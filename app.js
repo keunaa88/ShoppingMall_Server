e9ed4e3832c70119264478b0d8e45ee3f10637a3
Euna Kim <keunaa88@gmail.com>
2023-04-30 20:37:03 +1000

    test

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-// require("dotenv").config();
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const app = express();
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const post = require("./router/post");
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const post = require("./router/post");
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-// //deploy
-// const path = require('path');
-// app.use(express.static(path.join(__dirname, '../client/build')));
+//deploy
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 
 
-// const { MONGO_URI } = process.env;
+const { MONGO_URI } = process.env;
 
-// mongoose.connect(`${MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true,})
-//         .then(() => console.log('Successfully connected to mongodb'))
-//         .catch(e => console.error(e));
+mongoose.connect(`${MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true,})
+        .then(() => console.log('Successfully connected to mongodb'))
+        .catch(e => console.error(e));
 
-// const db = mongoose.connection;
-// app.locals.db = db;
+const db = mongoose.connection;
+app.locals.db = db;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
 
-// app.get("/api", (req, res) => {
-//   res.send("Hello World API!!");
-// });
+app.get("/api", (req, res) => {
+  res.send("Hello World API!!");
+});
 
-// app.use("/api/post", post)
+app.use("/api/post", post)
 
 
 
 const port = process.env.PORT || 8080;
 
-// app.listen(port, () => {
-//   console.log(`Server Listening on ${port}`);
-// });
-
-
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello Beanstalk!!");
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
 });
 
-app.listen(port); 
+
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Beanstalk!!");
+// });
+
+// app.listen(port); 
